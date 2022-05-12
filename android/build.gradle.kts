9e79bc43a8cf3da385e8beb10e4f9e1802f35244
Bren Pearson <bren.pearson0100@gmail.com>
2022-05-12 10:39:51 +1000

    Fix build for Jitpack

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,19 +1,13 @@
 plugins {
     id("com.android.library")
     kotlin("android")
-//    id("app.cash.paparazzi")
-
+    id("maven-publish")
 }
 
+val libVersion = "0.1.0"
 
-//gradlePlugin {
-//    plugins {
-//        create("strapp") {
-//            id = "au.strapp.strapp-ui"
-//            implementationClass = "app.cash.paparazzi.gradle.PaparazziPlugin"
-//        }
-//    }
-//}
+group = "com.github.strapp-au"
+version = libVersion
 
 android {
     compileSdk = 30
@@ -48,6 +42,30 @@ android {
     composeOptions {
         kotlinCompilerVersion = "1.6.10"
         kotlinCompilerExtensionVersion = "1.1.0-rc03"
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.github.strapp-au"
+                artifactId = "strapp-ui"
+                version = libVersion
+                description = libVersion
+
+                from(components["release"])
+            }
+            create<MavenPublication>("debug") {
+                from(components["debug"])
+            }
+        }
+        // note repositories goes under publishing
+        repositories {
+            maven {
+//                url = "file://$projectDir/deploy"
+            }
+        }
     }
 }
 
